bda0dcb31011eb7ea3d258b281fe7e48c9ef9690
mikey179 <mikey@stubbles.net>
2016-04-26 10:40:18 +0200

    Allow disabling of backup client installation
    Sometimes the backup client is not required because backups are
    solved in other ways. One could simply disable the cron for the
    backup, but when the client is not needed at all it is better to
    be able to completely disable the installation.

--- manifests/backup.pp
+++ manifests/backup.pp
@@ -4,6 +4,7 @@
 # This installs the bitbucket backup client
 #
 class bitbucket::backup(
+  $manage_backup        = $bitbucket::manage_backup,
   $ensure               = $bitbucket::backup_ensure,
   $schedule_hour        = $bitbucket::backup_schedule_hour,
   $schedule_minute      = $bitbucket::backup_schedule_minute,
@@ -22,88 +23,90 @@ class bitbucket::backup(
   $keep_age             = $bitbucket::backup_keep_age,
   ) {
 
-  $appdir = "${backup_home}/${product}-backup-client-${version}"
+  if $manage_backup {
+    $appdir = "${backup_home}/${product}-backup-client-${version}"
 
-  file { $backup_home:
-    ensure => 'directory',
-    owner  => $user,
-    group  => $group,
-  }
-  file { "${backup_home}/archives":
-    ensure => 'directory',
-    owner  => $user,
-    group  => $group,
-  }
+    file { $backup_home:
+      ensure => 'directory',
+      owner  => $user,
+      group  => $group,
+    }
+    file { "${backup_home}/archives":
+      ensure => 'directory',
+      owner  => $user,
+      group  => $group,
+    }
 
-  $file = "${product}-backup-distribution-${version}.${backup_format}"
+    $file = "${product}-backup-distribution-${version}.${backup_format}"
 
-  file { $appdir:
-    ensure => 'directory',
-    owner  => $user,
-    group  => $group,
-  }
-  
-  file { '/var/tmp/downloadurl':
-    content => "${download_url}/${version}/${file}",
-  }
+    file { $appdir:
+      ensure => 'directory',
+      owner  => $user,
+      group  => $group,
+    }
 
-  case $deploy_module {
-    'staging': {
-      require staging
-      staging::file { $file:
-        source  => "${download_url}/${version}/${file}",
-        timeout => 1800,
-      } ->
-      staging::extract { $file:
-        target  => $appdir,
-        creates => "${appdir}/lib",
-        strip   => 1,
-        user    => $user,
-        group   => $group,
-        require => [ User[$user], File[$appdir] ],
+    file { '/var/tmp/downloadurl':
+      content => "${download_url}/${version}/${file}",
+    }
+
+    case $deploy_module {
+      'staging': {
+        require staging
+        staging::file { $file:
+          source  => "${download_url}/${version}/${file}",
+          timeout => 1800,
+        } ->
+        staging::extract { $file:
+          target  => $appdir,
+          creates => "${appdir}/lib",
+          strip   => 1,
+          user    => $user,
+          group   => $group,
+          require => [ User[$user], File[$appdir] ],
+        }
+      }
+      'archive': {
+        archive { "/tmp/${file}":
+          ensure       => present,
+          extract      => true,
+          extract_path => $backup_home,
+          source       => "${download_url}/${version}/${file}",
+          user         => $user,
+          group        => $group,
+          creates      => "${appdir}/lib",
+          cleanup      => true,
+          before       => File[$appdir],
+        }
+      }
+      default: {
+        fail('deploy_module parameter must equal "archive" or staging""')
       }
     }
-    'archive': {
-      archive { "/tmp/${file}":
-        ensure       => present,
-        extract      => true,
-        extract_path => $backup_home,
-        source       => "${download_url}/${version}/${file}",
-        user         => $user,
-        group        => $group,
-        creates      => "${appdir}/lib",
-        cleanup      => true,
-        before       => File[$appdir],
-      }
+
+    if $javahome {
+      $java_bin = "${javahome}/bin/java"
+    } else {
+      $java_bin = '/usr/bin/java'
     }
-    default: {
-      fail('deploy_module parameter must equal "archive" or staging""')
+
+    # Enable Cronjob
+    $backup_cmd = "${java_bin} -Dbitbucket.password=\"${backuppass}\" -Dbitbucket.user=\"${backupuser}\" -Dbitbucket.baseUrl=\"http://localhost:7990\" -Dbitbucket.home=${homedir} -Dbackup.home=${backup_home}/archives -jar ${appdir}/bitbucket-backup-client.jar"
+
+    cron { 'Backup Bitbucket':
+      ensure  => $ensure,
+      command => $backup_cmd,
+      user    => $user,
+      hour    => $schedule_hour,
+      minute  => $schedule_minute,
     }
-  }
 
-  if $javahome {
-    $java_bin = "${javahome}/bin/java"
-  } else {
-    $java_bin = '/usr/bin/java'
-  }
-
-  # Enable Cronjob
-  $backup_cmd = "${java_bin} -Dbitbucket.password=\"${backuppass}\" -Dbitbucket.user=\"${backupuser}\" -Dbitbucket.baseUrl=\"http://localhost:7990\" -Dbitbucket.home=${homedir} -Dbackup.home=${backup_home}/archives -jar ${appdir}/bitbucket-backup-client.jar"
-
-  cron { 'Backup Bitbucket':
-    ensure  => $ensure,
-    command => $backup_cmd,
-    user    => $user,
-    hour    => $schedule_hour,
-    minute  => $schedule_minute,
-  }
-
-  tidy { 'remove_old_archives':
-    path    => "${backup_home}/archives",
-    age     => $keep_age,
-    matches => '*.tar',
-    type    => 'mtime',
-    recurse => 2,
+    tidy { 'remove_old_archives':
+      path    => "${backup_home}/archives",
+      age     => $keep_age,
+      matches => '*.tar',
+      type    => 'mtime',
+      recurse => 2,
+    }
   }
 
 }
