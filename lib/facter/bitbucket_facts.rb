600aa9d46a34fbdc919d9fdfb7c03578236a5068
Michael Speth <mspeth@monky-games.com>
2017-03-03 09:40:49 +1300

    Fixed puppet 4 open source incompatiblitiy.
    * Removed using the global fact directory.
    * Moved facts to the module 'lib' directory.
    * Added a configuration file in /etc.
    * Fails gracefully if fact is unavailable.

---
+++ lib/facter/bitbucket_facts.rb
@@ -0,0 +1,24 @@
+# Fact: bitbucket_builddate, bitbucket_buildnumber, bitbucket_displayname, bitbucket_version
+#
+# Purpose: Return facts for the running version of bitbucket.
+#
+require 'json'
+require 'open-uri'
+
+# get url of bitbucket
+file = File.open("/etc/bitbucket_url.txt", "rb")
+bitbucket_url = file.read
+begin
+  url = 'bitbucket_url'
+  info = open(url, &:read)
+rescue
+  exit 0
+end
+pinfo = JSON.load(info)
+pinfo.each do |key, value|
+  actual_value = value
+  if value.is_a? Array
+     actual_value = value.join(',')
+  end
+  puts "bitbucket_#{key.chomp()}=#{actual_value.chomp}"
+end
--- manifests/facts.pp
+++ manifests/facts.pp
@@ -22,37 +22,15 @@ class bitbucket::facts(
   $json_packages = $bitbucket::params::json_packages,
 ) inherits bitbucket {
 
-  # Puppet Enterprise supplies its own ruby version if your using it.
-  # A modern ruby version is required to run the executable fact
-  if $::puppetversion =~ /Puppet Enterprise/ {
-    $ruby_bin = '/opt/puppet/bin/ruby'
-    $dir      = 'puppetlabs/'
-  } else {
-    $ruby_bin = '/usr/bin/env ruby'
-    $dir      = ''
-  }
-
-  if ! defined(File["/etc/${dir}facter"]) {
-    file { "/etc/${dir}facter":
-      ensure  => directory,
-    }
-  }
-  if ! defined(File["/etc/${dir}facter/facts.d"]) {
-    file { "/etc/${dir}facter/facts.d":
-      ensure  => directory,
-    }
-  }
-
   if $::osfamily == 'RedHat' and $::puppetversion !~ /Puppet Enterprise/ {
     package { $json_packages:
       ensure => present,
     }
   }
 
-  file { "/etc/${dir}facter/facts.d/bitbucket_facts.rb":
+  file{'/etc/bitbucket_url.txt':
     ensure  => $ensure,
-    content => template('bitbucket/facts.rb.erb'),
-    mode    => '0500',
+    content => "http://${uri}:${port}${context_path}/rest/api/1.0/\
+application-properties",
   }
-
 }
